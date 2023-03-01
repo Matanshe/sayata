016a31d31d54d47bac3d2937aa876bc0b231f975
matan shemesh <matan.shemesh@zoominsoftware.com>
2023-03-01 20:33:35 +0200

    fix update and get submmision

--- client/client.py
+++ client/client.py
@@ -1,6 +1,6 @@
 import requests
 
-from submission_management.client.commands_parser import parse_submission_command
+import submission_management.client.commands_parser as parser
 
 BASE_URL = "http://localhost:5000"
 
@@ -22,13 +22,45 @@ def create_submission(company_name, physical_address, annual_revenue):
         print(response.json().get("error"))
 
 
+def update_submission(submission_id, company_name, physical_address, annual_revenue):
+    payload = {}
+    payload["company_name"] = company_name
+    payload["physical_address"] = physical_address
+    payload["annual_revenue"] = annual_revenue
+
+    response = requests.put(f"{BASE_URL}/submissions/update/{submission_id}", json=payload)
+    if response.status_code == 200:
+        print("Submission updated successfully")
+        history.append(f"update_submission {submission_id} {company_name} {physical_address} {annual_revenue}")
+    else:
+        print(response.json().get("error"))
+
+
+def get_submission(submission_id):
+    response = requests.get(f"{BASE_URL}/submissions/get/{submission_id}")
+    if response.status_code == 200:
+        submission_data = response.json()
+        print(submission_data)
+        history.append(f"get_submission {submission_id}")
+    else:
+        print(response.json().get("error"))
+
+
+
 if __name__ == '__main__':
     while True:
         command = input("Enter a command: ")
         try:
             if command.startswith("CREATE SUBMISSION"):
-                company_name, physical_address, annual_revenue = parse_submission_command(command)
+                company_name, physical_address, annual_revenue = parser.parse_create_submission_command(command)
                 create_submission(company_name, physical_address, annual_revenue)
+            elif command.startswith("UPDATE SUBMISSION"):
+                submission_id, company_name, physical_address, annual_revenue = parser.parse_update_submission_command(
+                    command)
+                update_submission(submission_id, company_name, physical_address, annual_revenue)
+            elif command.startswith("GET SUBMISSION"):
+                submission_id = command.split(" ")
+                get_submission(submission_id)
 
         except ValueError as e:
             print(e)
--- client/commands_parser.py
+++ client/commands_parser.py
@@ -1,6 +1,5 @@
-def parse_submission_command(command_string):
-    command_string = command_string.replace('”', '"')
-    command_string = command_string.replace('“', '"')
+def parse_create_submission_command(command_string):
+    command_string = fix_quotation_marks(command_string)
     parts = command_string.split('"')
     if len(parts) != 5:
         raise ValueError("Invalid command format")
@@ -9,3 +8,32 @@ def parse_submission_command(command_string):
     physical_address = parts[3]
     annual_revenue = int(parts[4].strip())
     return company_name, physical_address, annual_revenue
+
+
+def fix_quotation_marks(command_string):
+    command_string = command_string.replace('”', '"')
+    command_string = command_string.replace('“', '"')
+    return command_string
+
+
+def parse_update_submission_command(command_string):
+    command_string = fix_quotation_marks(command_string)
+    parts = command_string.split('"')
+    if len(parts) != 7:
+        raise ValueError("Invalid command format")
+
+    submission_id = parts[1]
+    company_name = parts[3]
+    physical_address = parts[4]
+    annual_revenue = int(parts[5].strip())
+    return company_name, physical_address, annual_revenue
+
+
+def parse_get_submission_command(command_string):
+    command_string = fix_quotation_marks(command_string)
+    parts = command_string.split('"')
+    if len(parts) != 4:
+        raise ValueError("Invalid command format")
+
+    submission_id = parts[1]
+    return submission_id
